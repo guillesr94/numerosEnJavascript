3054674f8c47dd8637a1129bde2fae2be72d6de5
Guillermo Suarez Reinoso <81838049+guillesr94@users.noreply.github.com>
2022-08-12 12:03:39 -0300

    Add files via upload

---
+++ NumerosEnJavascript.js
@@ -0,0 +1,15 @@
+// - Una variable que contenga tu altura en centímetros (entero)
+let altura = 175;
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+let alturaFlotante = 1.75;
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso = 71.05;
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaRedondeada = 2;
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoRedondeado = 71;
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let maxNum = Number.MAX_VALUE;
+let minNum = Number.MIN_VALUE;
+let comprobar = maxNum == minNum
+console.log(comprobar)
